996b76b48cbd3a46a46d390166c60ffc67f45877
Wookyum Kim <wookyum37@naver.com>
2017-09-17 16:02:08 +0900

    haha

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'colleted_statics')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'blog/static'),
     )
